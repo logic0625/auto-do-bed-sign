723f28647507e3faf062398b24bbad393cb46f56
ZhangWeiluo <zhangweiluo2005@outlook.com>
2025-10-30 23:25:37 +0800

    feat:优化邮箱返回结果

--- gzlg助手/emailSender.py
+++ gzlg助手/emailSender.py
@@ -36,7 +36,7 @@ def send_QQ_email_plain(content):
     msg['From'] = f'{sender}'
     msg['To'] = os.getenv('EMAIL_ADDRESS')
     # msg['To'] = '3552971348@qq.com'
-    msg['Subject'] = f'签到{result_status} {formatted_date}'  # 设置邮件主题
+    msg['Subject'] = f'查寝 {result_status} {formatted_date}'  # 设置邮件主题
 
     try:
         # 建立 SMTP 、SSL 的连接，连接发送方的邮箱服务器
